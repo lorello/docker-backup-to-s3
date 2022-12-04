ce1b072dea538aa5e7a54a01ac8f32fba3ee7cad
LoreLLo <lorenzo.salvadorini@opencontent.it>
2022-12-04 19:24:42 +0100

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,6 +4,14 @@ set -e
 
 echo "Job started: $(date)"
 
+[[ -n $HEALTHCHECK_ID ]] && curl -fsS -m 10 --retry 5 -o /dev/null https://hc-ping.com/${HEALTHCHECK_ID}/start
+
 /usr/local/bin/s3cmd sync $PARAMS "$DATA_PATH" "$S3_PATH"
+
+if [[ $? -eq 0 ]]; then
+  [[ -n $HEALTHCHECK_ID ]] && curl -fsS -m 10 --retry 5 -o /dev/null https://hc-ping.com/${HEALTHCHECK_ID}
+else
+  [[ -n $HEALTHCHECK_ID ]] && curl -fsS -m 10 --retry 5 -o /dev/null https://hc-ping.com/${HEALTHCHECK_ID}/fail
+fi
 
 echo "Job finished: $(date)"
